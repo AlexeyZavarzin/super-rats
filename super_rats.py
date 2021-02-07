16cbe7d1c19c85e1e6366373a6dee98e57213381
Alexey Zavarzin <alex.zavr.27100@main.ru>
2021-02-07 21:38:39 +0400

    reform

--- super_rats.py
+++ super_rats.py
@@ -4,14 +4,13 @@ import statistics
 from config import *
 
 
-def populate(num_rats, min_wt, max_wt, mode_wt):
+def get_populate(num_rats, min_wt, max_wt, mode_wt):
     return [int(random.triangular(min_wt, max_wt, mode_wt))
             for _ in range(num_rats)]
 
 
-def fitness(population, goal):
-    avg = statistics.mean(population)
-    return avg / goal
+def get_value_fitness(population, goal):
+    return statistics.mean(population) / goal
 
 
 def select(population, to_retain):
@@ -21,9 +20,9 @@ def select(population, to_retain):
     members_per_sex = len(sorted_population) // 2
 
     males = sorted_population[members_per_sex:]
-    females = sorted_population[:members_per_sex]
-
     selected_males = males[-to_retain_by_sex:]
+
+    females = sorted_population[:members_per_sex]
     selected_females = females[-to_retain_by_sex:]
 
     return selected_males, selected_females
@@ -52,11 +51,11 @@ def mutate(children, mutate_odds, mutate_min, mutate_max):
 
 def main():
     generations = 0
-    parents = populate(NUM_RATS, INITIAL_MIN_WT, INITIAL_MAX_WT,
-                       INITIAL_MODE_WT)
+    parents = get_populate(NUM_RATS, INITIAL_MIN_WT, INITIAL_MAX_WT,
+                           INITIAL_MODE_WT)
     print(f'первоначальные веса популяции = {parents}')
 
-    populations_fitness = fitness(parents, GOAL)
+    populations_fitness = get_value_fitness(parents, GOAL)
     print(f'первоначальная приспособленность популяции = {populations_fitness}')
     print(f'число крыс в начальной популяции = {NUM_RATS}')
 
@@ -67,7 +66,7 @@ def main():
         children = breed(selected_males, selected_females, LITTER_SIZE)
         children = mutate(children, MUTATE_ODDS, MUTATE_MIN, MUTATE_MAX)
         parents = selected_males + selected_females + children
-        populations_fitness = fitness(parents, GOAL)
+        populations_fitness = get_value_fitness(parents, GOAL)
 
         print(f'приспособленность поколения {generations} = {populations_fitness}')
         ave_wt.append(int(statistics.mean(parents)))
@@ -76,11 +75,3 @@ def main():
     print(f'средний вес на поколение = {ave_wt}')
     print(f'\nчисло поколений = {generations}')
     print(f'число лет {int(generations / LITTER_PER_YEAR)} ')
-
-
-if __name__ == '__main__':
-    start_time = time.time()
-    main()
-    end_time = time.time()
-    duration = end_time - start_time
-    print(f'\nВремя выполнения программы {duration} секунд')
